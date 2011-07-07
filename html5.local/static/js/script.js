0b6d50eb4d718d9cfa8ad874ecb62d87bb6790b6
wnas <wilfred@wnas.nl>
2011-07-07 17:51:42 +0200

    first commit

---
+++ html5.local/static/js/script.js
@@ -0,0 +1,15 @@
+/*
+	wilfred nas
+*/
+;var html5 = (function(){
+	var config = function(){
+		
+	}, init = function(){
+		// call all the stuff from here
+	};
+	return {
+		init: init
+	};	
+})();
+
+html5.init();
